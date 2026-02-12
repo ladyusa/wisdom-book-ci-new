d709b4a4e16beb4b5442582552a9c74424746479
Usa Sammapun <usa.sa@ku.th>
2026-02-13 01:11:12 +0700

    change greeting

--- src/main/java/th/ac/ku/book/controller/HomeController.java
+++ src/main/java/th/ac/ku/book/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        model.addAttribute("greeting", "Good Afternoon");
+        model.addAttribute("greeting", "Good Night");
         // return home.html
         return "home";
     }
